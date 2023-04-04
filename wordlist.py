85f82e9c1548c629e01abeeddc905bb5c3ee5cd6
Brian Barefoot Burns <3639671+bpburns@users.noreply.github.com>
2023-04-03 22:19:53 -0700

    Add wordlist.

---
+++ wordlist.py
@@ -0,0 +1,49 @@
+from collections import Counter
+import time
+import json
+
+with open('american-english-insane', 'r') as f:
+    english_words = f.read().splitlines()
+
+replacement_map = {
+    'Å': 'A',
+    'á': 'a', 
+    'â': 'a', 
+    'ä': 'a', 
+    'å': 'a', 
+    'ç': 'c', 
+    'è': 'e', 
+    'é': 'e', 
+    'ê': 'e', 
+    'í': 'i', 
+    'ñ': 'n', 
+    'ó': 'o', 
+    'ô': 'o', 
+    'ö': 'o', 
+    'û': 'u', 
+    'ü': 'u' 
+}
+
+# Remove words with punctuation
+upper_alphas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lower_alphas = upper_alphas.lower()
+alpha_characters = set(list(upper_alphas))
+alpha_characters.update(set(list(lower_alphas)))
+english_words_pruned = set()
+for word in english_words:
+
+    # Normalize to ASCII-like words.
+    for char in replacement_map.keys():
+        if char in word:
+            replacement = replacement_map[char]
+            word = word.replace(char, replacement)
+
+    charset = set(list(word))
+    if "'" in charset:
+        continue
+    if not charset - alpha_characters and len(word) > 2:
+        english_words_pruned.add(word.lower())
+
+sorted_word_list = sorted(english_words_pruned)
+
+WORD_LIST = sorted_word_list
